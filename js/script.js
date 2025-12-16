fea5aa6b1fbc0907e77859c290821dfd1a517e39
valecristiano <valeriamdcristiano@gmail.com>
2025-12-16 17:31:22 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Quanti km percorrerai?
+// Quanti anni hai?
+
+// prezzo biglietto 0.21 * km
+// -20% per minore di 18 anni
+// -40% per > di 65 anni
+
+//alert con prezzo finale con 2 decimali e simbolo €
